6c780be04f3c4c10a716313251bd2e9f014cba23
Bogdan <bogdan.pozderca@gmail.com>
2022-04-18 16:52:46 -0800

    Adds can_receive? method to Pile

--- game/pile.rb
+++ game/pile.rb
@@ -4,5 +4,19 @@ class Game
       super(cards)
       @is_foundation = is_foundation
     end
+
+    def can_receive?(card)
+      return false if card.nil?
+
+      if self.empty? && @is_foundation
+        card.value == Constants::RANKS[:ACE][1]
+      elsif self.empty?
+        card.value == Constants::RANKS[:KING][1]
+      elsif @is_foundation
+        self.last.value - 1 == card.value && self.last.symbol == card.symbol
+      else
+        self.last.value + 1 == card.value && self.last.color != card.color
+      end
+    end
   end
 end
